a623e9aecb5f48bdc642cd41530b75ea5dd5cf9f
dothithuthao <thaodo2512@gmail.com>
2021-01-08 15:18:43 +0700

    ver1.0

---
+++ plot_graphs/Form1.Designer.cs
@@ -0,0 +1,325 @@
+﻿
+namespace plot_graphs
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnPause = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.txtData = new System.Windows.Forms.TextBox();
+            this.btnDisConnect = new System.Windows.Forms.Button();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cboxBaudRate = new System.Windows.Forms.ComboBox();
+            this.cboxCom = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.zedGraphControl1 = new ZedGraph.ZedGraphControl();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "COM";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.groupBox1.Controls.Add(this.btnNext);
+            this.groupBox1.Controls.Add(this.btnSave);
+            this.groupBox1.Controls.Add(this.btnPause);
+            this.groupBox1.Controls.Add(this.btnClear);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.progressBar1);
+            this.groupBox1.Controls.Add(this.btnExit);
+            this.groupBox1.Controls.Add(this.txtData);
+            this.groupBox1.Controls.Add(this.btnDisConnect);
+            this.groupBox1.Controls.Add(this.btnConnect);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.cboxBaudRate);
+            this.groupBox1.Controls.Add(this.cboxCom);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 23);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(415, 161);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Config";
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(286, 85);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(75, 23);
+            this.btnNext.TabIndex = 14;
+            this.btnNext.Text = "Next";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(196, 117);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 13;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnPause
+            // 
+            this.btnPause.Location = new System.Drawing.Point(196, 85);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(75, 23);
+            this.btnPause.TabIndex = 12;
+            this.btnPause.Text = "Pause";
+            this.btnPause.UseVisualStyleBackColor = true;
+            this.btnPause.Click += new System.EventHandler(this.btnPause_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(100, 117);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 11;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(202, 61);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "progressBar1";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(277, 49);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(121, 23);
+            this.progressBar1.TabIndex = 9;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(100, 85);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 8;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // txtData
+            // 
+            this.txtData.Location = new System.Drawing.Point(277, 20);
+            this.txtData.Name = "txtData";
+            this.txtData.Size = new System.Drawing.Size(121, 20);
+            this.txtData.TabIndex = 5;
+            // 
+            // btnDisConnect
+            // 
+            this.btnDisConnect.Location = new System.Drawing.Point(9, 117);
+            this.btnDisConnect.Name = "btnDisConnect";
+            this.btnDisConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnDisConnect.TabIndex = 7;
+            this.btnDisConnect.Text = "Disconnect";
+            this.btnDisConnect.UseVisualStyleBackColor = true;
+            this.btnDisConnect.Click += new System.EventHandler(this.btnDisConnect_Click);
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(9, 83);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 6;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(202, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Data";
+            // 
+            // cboxBaudRate
+            // 
+            this.cboxBaudRate.FormattingEnabled = true;
+            this.cboxBaudRate.Items.AddRange(new object[] {
+            "110",
+            "300",
+            "600",
+            "1200",
+            "2400",
+            "4800",
+            "9600",
+            "14400",
+            "19200",
+            "38400",
+            "57600",
+            "115200",
+            "230400",
+            "460800",
+            "921600"});
+            this.cboxBaudRate.Location = new System.Drawing.Point(66, 51);
+            this.cboxBaudRate.Name = "cboxBaudRate";
+            this.cboxBaudRate.Size = new System.Drawing.Size(121, 21);
+            this.cboxBaudRate.TabIndex = 3;
+            // 
+            // cboxCom
+            // 
+            this.cboxCom.FormattingEnabled = true;
+            this.cboxCom.Location = new System.Drawing.Point(66, 19);
+            this.cboxCom.Name = "cboxCom";
+            this.cboxCom.Size = new System.Drawing.Size(121, 21);
+            this.cboxCom.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Baud rate";
+            // 
+            // zedGraphControl1
+            // 
+            this.zedGraphControl1.Location = new System.Drawing.Point(433, 23);
+            this.zedGraphControl1.Name = "zedGraphControl1";
+            this.zedGraphControl1.ScrollGrace = 0D;
+            this.zedGraphControl1.ScrollMaxX = 0D;
+            this.zedGraphControl1.ScrollMaxY = 0D;
+            this.zedGraphControl1.ScrollMaxY2 = 0D;
+            this.zedGraphControl1.ScrollMinX = 0D;
+            this.zedGraphControl1.ScrollMinY = 0D;
+            this.zedGraphControl1.ScrollMinY2 = 0D;
+            this.zedGraphControl1.Size = new System.Drawing.Size(1081, 637);
+            this.zedGraphControl1.TabIndex = 2;
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 190);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(415, 470);
+            this.listView1.TabIndex = 3;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Thời gian (s)";
+            this.columnHeader1.Width = 150;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Dữ liệu";
+            this.columnHeader2.Width = 150;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1537, 707);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.zedGraphControl1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnDisConnect;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.TextBox txtData;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cboxBaudRate;
+        private System.Windows.Forms.ComboBox cboxCom;
+        private System.Windows.Forms.Label label2;
+        private ZedGraph.ZedGraphControl zedGraphControl1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnPause;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+    }
+}
+
---
+++ plot_graphs/Form1.cs
@@ -0,0 +1,283 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.IO;
+using System.Xml;
+using ZedGraph;
+
+namespace plot_graphs
+{
+    public partial class Form1 : Form
+    {
+        string DataIn="";
+        string temp1="", temp2="";
+        double datanumber=0.0, realtime=0.0;
+        int tickStart=0;
+        int status = 0;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Khi nhấn exit 
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+
+        // Khi nhấn kết nối
+        private void btnConnect_Click(object sender, EventArgs e)
+        {   // cấu hình lấy dữ liệu từ com và baudrate cho cổng serial, ko có cái này ko chạy dc
+            serialPort1.PortName = cboxCom.Text;
+            serialPort1.BaudRate = Convert.ToInt32(cboxBaudRate.Text);
+
+            if (serialPort1.IsOpen) return;
+            serialPort1.Open();
+            btnConnect.Enabled = false;
+            btnDisConnect.Enabled = true;
+            txtData.Text = temp2;
+
+        }
+
+
+        // Khi nhấn ngắt kết nối 
+        private void btnDisConnect_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen == false) return;
+            serialPort1.Close();
+            btnConnect.Enabled = true;
+            btnDisConnect.Enabled = false;
+            txtData.Text = "";
+        }
+
+        // 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] ports = SerialPort.GetPortNames();
+            cboxCom.Items.AddRange(ports);
+            // khoi tao zedgraph
+            GraphPane myPane = zedGraphControl1.GraphPane;
+            myPane.Title.Text = "Đồ thị dữ liệu theo thời gian";
+            myPane.XAxis.Title.Text = "Thời gian (s)";
+            myPane.YAxis.Title.Text = "Dữ Liệu";
+
+            RollingPointPairList list = new RollingPointPairList(60000);
+            timer1.Interval = 1;// 1ms gửi lên 1 lần 
+            timer1.Start();// chayj timer 1
+            LineItem curve = myPane.AddCurve("Dữ liệu", list, Color.Blue, SymbolType.None);
+
+            myPane.XAxis.Scale.Min = 0;
+            myPane.XAxis.Scale.Max = 30;
+            myPane.XAxis.Scale.MinorStep = 1;
+            myPane.XAxis.Scale.MajorStep = 5;
+            myPane.YAxis.Scale.Min = -100;
+            myPane.YAxis.Scale.Max = 100;
+
+            curve.Line.Width = 3;
+
+            myPane.AxisChange();// gọi hàm xác định cỡ trục
+            tickStart = Environment.TickCount;// khởi động timer vể vị trí ban đầu 
+
+            
+
+
+        }
+
+        private void Draw()
+        {
+            if (zedGraphControl1.GraphPane.CurveList.Count <= 0)
+                return;
+            // kiểm tra việc khởi tạo các đường curve
+            // đưa vể điểm xuất phát 
+
+            LineItem curve = zedGraphControl1.GraphPane.CurveList[0] as LineItem;
+            if (curve == null)
+                return;
+            // list các điểm 
+            IPointListEdit list = curve.Points as IPointListEdit;
+
+            if (list == null)
+                return;
+            // realtime được tính bắng ms
+            realtime = (Environment.TickCount - tickStart) / 1000.0;
+
+            list.Add(realtime, datanumber); // hàm hiển thị dữ liệu trên đồ thị 
+
+            
+
+            Scale xScale = zedGraphControl1.GraphPane.XAxis.Scale;
+            Scale yScale = zedGraphControl1.GraphPane.YAxis.Scale;
+
+            // Tự động Scale theo trục x
+            if (realtime > xScale.Max - xScale.MajorStep)
+            {
+                xScale.Max = realtime + xScale.MajorStep;
+                xScale.Min = xScale.Max - 30;
+            }
+
+
+            // Tự động Scale theo trục y
+            if (datanumber > yScale.Max - yScale.MajorStep)
+            {
+                yScale.Max = datanumber + yScale.MajorStep;
+            }
+            else if (datanumber < yScale.Min + yScale.MajorStep)
+            {
+                yScale.Min = datanumber - yScale.MajorStep;
+            }
+
+            zedGraphControl1.AxisChange();
+            zedGraphControl1.Invalidate();
+            zedGraphControl1.Refresh();
+        }
+
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            string[] temp = serialPort1.ReadLine().Split('a');// đọc một dòng của serial, cắt chuỗi khi gặp kí tự a
+            temp1 = temp[0];// Chuỗi đầu tiên lưu vào temp1
+            temp2 = temp[1];// Chuỗi thứ hai lưu vào temp2
+
+            double.TryParse(temp2, out datanumber);// chuyển sang kiểu double;
+
+
+            status = 1;
+            this.Invoke(new EventHandler(ShowData));
+
+        }
+
+        private void btnPause_Click(object sender, EventArgs e)
+        {
+            serialPort1.Close();
+       
+
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            serialPort1.Open();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            
+            datanumber = 0;
+            temp2 = "";
+            status = 0;
+            ClearZedGraph();
+            listView1.Items.Clear();
+            txtData.Text = "0";
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if(!serialPort1.IsOpen)
+            {
+                progressBar1.Value = 0;
+            }
+            else if (serialPort1.IsOpen)
+            {
+                progressBar1.Value = 100;
+                Draw();
+                Data_Listview();
+                status = 0;
+            }
+
+        }
+
+        private void ShowData(object sender, EventArgs e)
+        {
+            txtData.Text = temp2;
+            
+        }
+        // Xóa đồ thị, với ZedGraph thì phải khai báo lại như ở hàm Form1_Load, nếu không sẽ không hiển thị
+
+        private void ClearZedGraph()
+        {
+            zedGraphControl1.GraphPane.CurveList.Clear(); // Xóa đường
+            zedGraphControl1.GraphPane.GraphObjList.Clear(); // Xóa đối tượng
+
+            zedGraphControl1.AxisChange();
+            zedGraphControl1.Invalidate();
+
+            GraphPane myPane = zedGraphControl1.GraphPane;
+            myPane.Title.Text = "Đồ thị dữ liệu theo thời gian";
+            myPane.XAxis.Title.Text = "Thời gian (s)";
+            myPane.YAxis.Title.Text = "Dữ liệu";
+
+            RollingPointPairList list = new RollingPointPairList(60000);
+            LineItem curve = myPane.AddCurve("Dữ liệu", list, Color.Blue, SymbolType.None);
+
+            myPane.XAxis.Scale.Min = 0;
+            myPane.XAxis.Scale.Max = 30;
+            myPane.XAxis.Scale.MinorStep = 1;
+            myPane.XAxis.Scale.MajorStep = 5;
+            myPane.YAxis.Scale.Min = -100;
+            myPane.YAxis.Scale.Max = 100;
+            curve.Line.Width = 3;
+
+            zedGraphControl1.AxisChange();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            SaveToExcel();
+        }
+
+        // Hiển thị dữ liệu trong ListView
+        private void Data_Listview()
+        {
+            if (status == 0)
+                return;
+            else
+            {
+                ListViewItem item = new ListViewItem(realtime.ToString()); // Gán biến realtime vào cột đầu tiên của ListView
+                item.SubItems.Add(datanumber.ToString());
+                listView1.Items.Add(item); // Gán biến datas vào cột tiếp theo của ListView
+                                           // Không nên gán string SDatas vì khi xuất dữ liệu sang Excel sẽ là dạng string, không thực hiện các phép toán được
+
+                listView1.Items[listView1.Items.Count - 1].EnsureVisible(); // Hiện thị dòng được gán gần nhất ở ListView, tức là mình cuộn ListView theo dữ liệu gần nhất đó
+            }
+        }
+
+        // Hàm lưu ListView sang Excel
+private
+    void SaveToExcel()
+    {
+        Microsoft.Office.Interop.Excel.Application xla = new Microsoft.Office.Interop.Excel.Application();
+        xla.Visible = true;
+        Microsoft.Office.Interop.Excel.Workbook wb = xla.Workbooks.Add(Microsoft.Office.Interop.Excel.XlSheetType.xlWorksheet);
+        Microsoft.Office.Interop.Excel.Worksheet ws = (Microsoft.Office.Interop.Excel.Worksheet)xla.ActiveSheet;
+ 
+        // Đặt tên cho hai ô A1. B1 lần lượt là "Thời gian (s)" và "Dữ liệu", sau đó tự động dãn độ rộng
+        Microsoft.Office.Interop.Excel.Range rg = (Microsoft.Office.Interop.Excel.Range)ws.get_Range("A1", "B1");
+        ws.Cells[1, 1] = "Thời gian (s)";
+        ws.Cells[1, 2] = "Dữ liệu";
+        rg.Columns.AutoFit();
+ 
+        // Lưu từ ô đầu tiên của dòng thứ 2, tức ô A2
+        int i = 2;
+        int j = 1;
+ 
+        foreach (ListViewItem comp in listView1.Items) {
+            ws.Cells[i, j] = comp.Text.ToString();
+            foreach (ListViewItem.ListViewSubItem drv in comp.SubItems) {
+                ws.Cells[i, j] = drv.Text.ToString();
+                j++;
+            }
+            j = 1;
+            i++;
+        }
+    }
+
+    }
+}
